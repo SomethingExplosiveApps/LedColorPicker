a57198705378e8b8d60e992b85358d8ad3669b86
Michael Limb <something15525@gmail.com>
2016-07-10 20:43:31 -0700

    mobile: Initial commit.

---
+++ mobile/src/main/java/com/somexapps/ledcolorpicker/LedColorPickerActivity.java
@@ -0,0 +1,13 @@
+package com.somexapps.ledcolorpicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LedColorPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_led_color_picker);
+    }
+}
