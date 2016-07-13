018dff0121e4b569cf69931ce10831f6ffa36279
Michael Limb <something15525@gmail.com>
2016-07-12 20:31:55 -0700

    Add initial color picker in mobile app.

--- mobile/src/main/java/com/somexapps/ledcolorpicker/LedColorPickerActivity.java
+++ mobile/src/main/java/com/somexapps/ledcolorpicker/LedColorPickerActivity.java
@@ -1,13 +1,95 @@
 package com.somexapps.ledcolorpicker;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.pavelsikun.vintagechroma.ChromaDialog;
+import com.pavelsikun.vintagechroma.ChromaUtil;
+import com.pavelsikun.vintagechroma.IndicatorMode;
+import com.pavelsikun.vintagechroma.colormode.ColorMode;
+import com.somexapps.ledcolorpicker.utils.Constants;
 
 public class LedColorPickerActivity extends AppCompatActivity {
+    // Used to store the current color that is set
+    private int currentColor;
+
+    // Shared prefs for holding the saved color value
+    private SharedPreferences preferences;
+
+    // Current color text view
+    private TextView currentColorTextView;
+
+    // Current color view
+    private View currentColorView;
+
+    // Used to identify the color picker dialog
+    private static final String DIALOG_COLOR_PICKER = "dialog_color_picker";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led_color_picker);
+
+        // Grab views and set click listeners
+        currentColorTextView =
+                (TextView) findViewById(R.id.activity_led_color_picker_current_color_text);
+        Button updateColorButton =
+                (Button) findViewById(R.id.activity_led_color_picker_new_color_button);
+
+        preferences = getPreferences(Context.MODE_PRIVATE);
+
+        // Grab color view to set current color
+        currentColorView = findViewById(R.id.activity_led_color_picker_current_color_view);
+
+        // Update the current color with saved color, if it exists
+        int savedColor = preferences.getInt(
+                Constants.PREF_SAVED_COLOR_VALUE,
+                Constants.DEFAULT_SAVED_COLOR_VALUE
+        );
+
+        // Update color on the tv
+        updateColor(savedColor);
+
+        // Set up button click
+        updateColorButton.setOnClickListener(view -> new ChromaDialog.Builder()
+                .initialColor(savedColor)
+                .colorMode(ColorMode.RGB) // RGB, ARGB, HVS, CMYK, CMYK255, HSL
+                .indicatorMode(IndicatorMode.DECIMAL) //HEX or DECIMAL; Note that (HSV || HSL || CMYK) && IndicatorMode.HEX is a bad idea
+                // Set color text view when selected
+                .onColorSelected(this::updateColor)
+                .create()
+                .show(getSupportFragmentManager(), DIALOG_COLOR_PICKER));
+    }
+
+    private void updateColor(int newColor) {
+        // Update color
+        currentColor = newColor;
+
+        // Update text view with new color
+        currentColorTextView.setText(
+                getString(
+                        R.string.activity_led_color_picker_current_color_header,
+                        ChromaUtil.getFormattedColorString(currentColor, false)
+                )
+        );
+
+        // Update color view
+        currentColorView.setBackgroundColor(newColor);
+
+        // Save color in shared preferences
+        preferences
+                .edit()
+                .putInt(Constants.PREF_SAVED_COLOR_VALUE, newColor)
+                .apply();
+
+        // Make sure we have a saved color
+        if (currentColor != 0) {
+            // TODO: Update tv with color
+        }
     }
 }
---
+++ mobile/src/main/java/com/somexapps/ledcolorpicker/utils/Constants.java
@@ -0,0 +1,27 @@
+package com.somexapps.ledcolorpicker.utils;
+
+/**
+ * Created by Michael Limb on 7/10/2016.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class Constants {
+    /**
+     * Used to save the last picked color value in Shared Preferences.
+     */
+    public static final String PREF_SAVED_COLOR_VALUE = "pref_saved_color_value";
+
+    /**
+     * The default value used for the color picker.
+     */
+    public static final int DEFAULT_SAVED_COLOR_VALUE = -15132304;
+}
